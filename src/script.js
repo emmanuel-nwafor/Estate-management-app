5a279c7addbb0f152ad002c05eb83a106a8cea77
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-04-15 18:54:17 -0700

    all commit

--- src/script.js
+++ src/script.js
@@ -1,6 +1,15 @@
 const toggleBtn = document.getElementById('menuToggle');
 const mobileMenu = document.getElementById('mobileMenu');
 
+let menuOpen = false;
+
 toggleBtn.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden');
+  menuOpen = !menuOpen;
+  if (menuOpen) {
+    mobileMenu.classList.add('menu-active');
+    mobileMenu.classList.remove('menu-enter');
+  } else {
+    mobileMenu.classList.add('menu-enter');
+    mobileMenu.classList.remove('menu-active');
+  }
 });
